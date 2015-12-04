8f1d4b770356e33cc32c2e4b797528e7d25235f9
Yangsibai <yangsibai@gmail.com>
2015-12-04 23:15:41 +0800

    first commit

---
+++ GeekNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GeekNote
+//
+//  Created by yangsibai on 15/12/4.
+//  Copyright © 2015年 massimo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GeekNote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeekNote
+//
+//  Created by yangsibai on 15/12/4.
+//  Copyright © 2015年 massimo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
